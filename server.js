4ebdcc0084369e14c0e0809e9f3681be10abc098
lux768 <luguimgiacomitti@gmail.com>
2025-09-22 11:13:48 -0300

    Tkinter

--- server.js
+++ server.js
@@ -1,15 +1,5 @@
-const http = require('http');
-
-// Create a server
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!\n');
-});
-
-// Define a port
-const PORT = 3000;
-
-// Start the server
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+let http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+}).listen(8080);
